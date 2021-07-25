6e3a15aab7930b5f11d816b81e8feed06984b430
cherhchen <54284307+cherhchen@users.noreply.github.com>
2021-07-24 19:05:46 -0700

    Create conditional-activity.py

---
+++ Week 2/conditionals-activity.py
@@ -0,0 +1,7 @@
+#boolean activity
+answer = input("Have already taken Calculus I? \n")
+
+if answer == "yes":
+    print("Great! You can move on to Calculus II next semester.")
+else:
+    print("Awesome! You will be taking Calculus I next semester.")
